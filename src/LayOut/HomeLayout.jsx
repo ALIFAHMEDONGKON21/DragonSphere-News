dda9d040ed610261d925c090d6ef0697bd06e5dc
MD ALIF <mdalifahmed114510@gmail.com>
2024-11-23 23:53:24 +0600

    set up done

---
+++ src/LayOut/HomeLayout.jsx
@@ -0,0 +1,18 @@
+import Header from "../components/Header";
+
+
+const HomeLayout = () => {
+    return (
+        <div>
+            <h1>Here all </h1>
+            <header>
+            <Header></Header>
+            </header>
+            <nav>
+
+            </nav>
+        </div>
+    );
+};
+
+export default HomeLayout;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import headerphot from '../assets/logo.png'
+
+const Header = () => {
+    return (
+        <div>
+            <img src={headerphot} alt="" />
+        </div>
+    );
+};
+
+export default Header;
